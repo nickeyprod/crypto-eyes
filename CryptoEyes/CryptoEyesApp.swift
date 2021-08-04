ad4a6b98a920dd3fa96b89e7dc57af5814e23dd5
Nickeyprod <nickeyprod@gmail.com>
2021-08-04 20:47:13 +0300

    Initial Commit

---
+++ CryptoEyes/CryptoEyesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoEyesApp.swift
+//  CryptoEyes
+//
+//  Created by Николай Ногин on 04.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct CryptoEyesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
